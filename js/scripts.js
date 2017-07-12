2d9df316c1186db3afdcf5b3780c42a7a160ab5c
Bart Webber and Carson Canales <carson@>
2017-07-12 15:32:30 -0700

    declare variable seperator as a space to use in the split function

--- js/scripts.js
+++ js/scripts.js
@@ -12,8 +12,11 @@ $(document).ready(function(){
   $("form#pig-latin").submit(function(event){
     event.preventDefault();
     //line below assigns the variable regularText to equal the input value regualrText(from the form imput id = regularText)
+
+    //declaired a " " as the variable seperator to use to split input into an array of words 
+    var seperator = (" ");
     var regularTextInput = $("input#regularText").val();
-    var regularTextArray = regularTextInput.split(" ");
+    var regularTextArray = regularTextInput.split(seperator);
     alert(regularTextArray);
     $(".regularText").text(regularTextInput);
     $("#result").show();
